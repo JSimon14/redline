113b7adcdc3e0e251da77a8dafdd9ae5b6f0262b
Julia Solstice Simon <julia.solstice.simon@gmail.com>
2018-04-10 21:48:00 -0400

    Adding new static pages for about us and protests

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,4 +3,12 @@ class StaticPagesController < ApplicationController
   def terms_and_conditions
 
   end
+
+  def about_us
+
+  end
+
+  def protests
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :volunteers, :only => [:index, :new, :create]
   get 'terms_and_conditions', to:'static_pages#terms_and_conditions'
+  get 'about_us', to:'static_pages#about_us'
+  get 'protests', to:'static_pages#protests'
   root 'volunteers#new'
 end
