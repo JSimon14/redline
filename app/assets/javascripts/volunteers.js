7a85e539013f03ea3f322c7e3c08e9d14d5c1ba8
Andrei Sarabia <sachandre194@protonmail.com>
2018-04-15 19:52:56 -0700

    before text added to terms and conditions

---
+++ app/assets/javascripts/volunteers.js
@@ -0,0 +1,8 @@
+'use strict'
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'))
