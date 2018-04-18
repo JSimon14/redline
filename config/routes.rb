4fcf206385bb3f0034da6507163f791bee3cc3e0
Andrei Sarabia <sachandre194@protonmail.com>
2018-04-17 20:27:58 -0700

    privacy page made, civic logo added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :volunteers, :only => [:index, :new, :create]
-  get 'terms_and_conditions', to:'static_pages#terms_and_conditions'
+  get 'privacy_policy', to:'static_pages#privacy_policy'
   get 'about_us', to:'static_pages#about_us'
   get 'protests', to:'static_pages#protests'
   root 'volunteers#new'
